098d253b048331b55f083b69c5e4e65e935769a5
Noe Ceron <77555935+noefernando@users.noreply.github.com>
2024-11-22 13:38:14 -0600

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,5 +1,10 @@
 function ingreso() {
+    var url = `https://www.youtube.com/watch?v=1RKqOmSkGgM&list=RD1RKqOmSkGgM&start_radio=1&autoplay=1`;
+    var elemento = document.getElementsByClassName("uwu")[0];
 
-    var url = `https://www.youtube.com/watch?v=1RKqOmSkGgM&list=RD1RKqOmSkGgM&start_radio=1; autoplay`
-    document.getElementsByClassName("uwu")[0].src = url
+    if (elemento) {
+        elemento.src = url;
+    } else {
+        console.error("No se encontró un elemento con la clase 'uwu'.");
+    }
 }
